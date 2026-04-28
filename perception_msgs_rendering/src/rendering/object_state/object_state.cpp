86b57eb5a3b66279d8452c46a79f149339fb7e24
Emre Karincali <emre.karincali@ika.rwth-aachen.de>
2026-04-28 16:45:30 +0000

    fix velocity visualization

--- perception_msgs_rendering/src/rendering/object_state/object_state.cpp
+++ perception_msgs_rendering/src/rendering/object_state/object_state.cpp
@@ -650,6 +650,7 @@ void ObjectState::setObjectPredictionsVizDefault(
         tf2::fromMsg(perception_msgs::object_access::getPose(states[i]), state_tf);
         auto transformed_pos_tf = base_state_tf.inverse() * state_tf;
         auto pos = transformed_pos_tf.getOrigin();
+        pos.setZ(static_cast<float>(perception_msgs::object_access::getVelocityMagnitude(states[i])));
         auto orientation = transformed_pos_tf.getRotation();
         bbox_prediction[i]->setPosition(Ogre::Vector3(pos.x(), pos.y(), pos.z()));
         bbox_prediction[i]->setOrientation(
